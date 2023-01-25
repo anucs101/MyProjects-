ce810a7d6a34e128a634ad327f9d4b6125e19fff
anucs101 <anucs101@gmail.com>
2023-01-25 10:57:23 +0530

    my projects

---
+++ MyProjects/src/test/java/test/Test1.java
@@ -0,0 +1,61 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{	
+		int sum =0;
+		for(int i=1;i<=10;i++)
+		{
+			sum = sum +i;
+			
+		}
+		System.out.println(sum);
+		summing();
+		int i =0;
+		do
+		{
+			System.out.println("hghg");
+		}
+		while(i>10);
+		
+	//	for(int i =0;i>10;i++)
+		{
+			if(i==5)
+			{
+	//			continue;
+			}
+		}
+		
+		int array[] = new int[5];
+		array[0]= 1;
+		array[1]= 2;
+		int arr[] = {1,2,3,4};
+		for(int q=0;q<=4;q++)
+		{
+			sum = sum + arr[q];
+		}
+			
+			
+		
+	}
+	
+	public static void summing()
+	{
+	int j = 1;
+	int sum1 = 0;
+	while(j<=20)
+	{
+		j = j++;
+		sum1 = sum1 + j;
+		
+	}
+	System.out.println(sum1);
+	}
+	
+	
+		
+
+	
+
+}
